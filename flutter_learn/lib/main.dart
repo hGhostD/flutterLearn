b75dbe79d03f44beb5423d843abfa3ccf0843bf9
hGhostD <644727408@qq.com>
2019-04-19 21:40:06 +0800

    使用变量的形式创建组件 学习 stack 叠加方式放置组件

--- flutter_learn/lib/main.dart
+++ flutter_learn/lib/main.dart
@@ -5,22 +5,29 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var stack = new Stack(
+      alignment: const FractionalOffset(0.5, 0.8),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new NetworkImage('https://avatars3.githubusercontent.com/u/13240543?s=460&v=4'),
+          radius: 100,
+        ),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Colors.lightBlueAccent
+          ),
+          padding: EdgeInsets.all(5),
+          child: Text('Stack 组件'),
+        )
+      ],
+    );
     return MaterialApp(
       title: "Row Widget",
       home: Scaffold(
         appBar: new AppBar(
           title: new Text('垂直方向布局'),
         ),
-        body: Center(child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('这是第一列'),
-            Text('This is second.'),
-            Expanded(child: Text('第二列文字可以显示在这里'),),
-            Text('Flutter Leanr&Github'),
-          ],
-        ),)
+        body: Center(child: stack,),
       ),
     );
   }
